545ed0ea5d1b9d1e3cee6fbc10c93138f1f1b211
vuongj321 <jasonvuong04@gmail.com>
2025-07-08 18:58:12 -0500

    Implement search

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,22 +1,43 @@
 import React, { useState, useEffect } from "react";
 import MovieCard from "../components/MovieCard";
-import { getPopularMovies } from "../services/api";
+import { getPopularMovies, searchMovie } from "../services/api";
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const loadPopularMovies = async () => {
-      setMovies(await getPopularMovies());
+      try {
+        setMovies(await getPopularMovies());
+      } catch (err) {
+        console.log(err);
+        setError("Failed to load movies");
+      } finally {
+        setLoading(false);
+      }
     };
 
     loadPopularMovies();
   }, []);
 
-  const handleSearch = (e) => {
+  const handleSearch = async (e) => {
     e.preventDefault();
-    alert(searchQuery);
+    if (!searchQuery.trim()) return;
+    if (loading) return;
+
+    setLoading(true);
+    try {
+      const searchResult = await searchMovie(searchQuery);
+      setMovies(searchResult);
+    } catch (err) {
+      console.log(err);
+      setError("Failed to search for movie");
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -32,11 +53,18 @@ const Home = () => {
           <button className="border-2 p-1 m-1">Search</button>
         </form>
       </div>
-      <div className="flex justify-center flex-wrap">
-        {movies.map((movie) => (
-          <MovieCard movie={movie} key={movie.id} />
-        ))}
-      </div>
+
+      {error && <div>{error}</div>}
+
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className="flex justify-center flex-wrap">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
